68685082b863f7dc88d0c5adb5729e1a74a7bce4
Holden Grissett <holdeng96@gmail.com>
2017-03-21 22:50:00 +0000

    add dev database setup file

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Create a new development database and user
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_db.* TO 'hbnb_dev'@'localhost'
