4000697681f709f0b3c188f70514a050c9032394
Holden Grissett <holdeng96@gmail.com>
2017-03-22 04:46:21 +0000

    make cities work with SQL

--- models/city.py
+++ models/city.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import Base
 
 
-class City(BaseModel):
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    __tablename__ = 'cities'
+    state_id = Column(String(60), nullable=False)
+    name = Column(String(128), ForeignKey('states.id'), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
