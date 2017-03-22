d3d2112d7dd66b1691c82626fcff7a2376117228
Holden Grissett <holdeng96@gmail.com>
2017-03-22 04:46:37 +0000

    make reviews work with SQL

--- models/review.py
+++ models/review.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import Base
 
 
-class Review(BaseModel):
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), Foreign('users.id'), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
