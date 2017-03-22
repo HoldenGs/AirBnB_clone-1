91206bb6f75429d6093c28688ecbb3110c1d6466
Holden Grissett <holdeng96@gmail.com>
2017-03-22 04:46:54 +0000

    make states work with SQL

--- models/state.py
+++ models/state.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, orm.relationship
+from models.base_model import Base
 
 
-class State(BaseModel):
-    name = ""
+class State(BaseModel, Base):
+    __tablename = 'states'
+    name = Column(String(128), nullable=False)
+    cities = orm.relationship('City', back_populates='state')
 
     def __init__(self, *args, **kwargs):
         super(State, self).__init__(*args, **kwargs)
