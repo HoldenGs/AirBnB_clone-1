f5b41870cbc67451393e2da377e821f87565b0bd
Holden Grissett <holdeng96@gmail.com>
2017-03-22 04:47:09 +0000

    make users work with SQL

--- models/user.py
+++ models/user.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, orm.relationship
+from models.base_model import Base
 
 
-class User(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = orm.relationship('Place', back_populates='user')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
