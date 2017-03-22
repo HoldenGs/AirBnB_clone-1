85f2362df11f79302c4b9799ab3109807c43c365
Holden Grissett <holdeng96@gmail.com>
2017-03-22 04:46:06 +0000

    make amenities work with SQL

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from models.base_model import Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String, nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
