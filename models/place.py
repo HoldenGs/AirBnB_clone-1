3bdff753f09208ed68600a87f8fab94350b43ed0
Holden Grissett <holdeng96@gmail.com>
2017-03-22 04:48:08 +0000

    add a new class called PlaceAmenity and make Place and PlaceAmenity SQL friendly

--- models/place.py
+++ models/place.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, Integer, Float, orm.relationship
+from sqlalchemy import ForeignKey
+from models.base_model import Base
 
 
-class Place(BaseModel):
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenities = [""]
+class PlaceAmenity(Base):
+    __tablename__ = 'place_amenity'
+    place_id = Column(String(60), primary_key=True,
+                      ForeignKey('places.id'), nullable=False)
+    amenity_id = Column(String(60), primary_key=True,
+                        ForeignKey('amenities.id'), nullable=False)
+
+
+class Place(BaseModel, Base):
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    amenities = orm.relationship('Amenity', secondary='place_amenity',
+                                 viewonly=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__()
