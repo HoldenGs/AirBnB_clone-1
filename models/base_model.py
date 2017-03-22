c119f01058d56e607152ccf09e784121288322da
Holden Grissett <holdeng96@gmail.com>
2017-03-22 03:56:35 +0000

    modify BaseModel to work with new database setup

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-import datetime
+from datetime import datetime
 import uuid
 import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """The base class for all storage objects in this project"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """initialize class object"""
+        if len(kwargs) > 0:
+            for key, value in kwargs:
+                setattr(self, key, value)
         if len(args) > 0:
             for k in args[0]:
                 setattr(self, k, args[0][k])
         else:
-            self.created_at = datetime.datetime.now()
+            self.created_at = datetime.now()
             self.id = str(uuid.uuid4())
         for k in kwargs:
             print("kwargs: {}: {}".format(k, kwargs[k]))
 
     def save(self):
         """method to update self"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
+
+    def delete(self):
+        models.storage.delete(self)
 
     def __str__(self):
         """edit string representation"""
@@ -31,6 +45,7 @@ class BaseModel:
     def to_json(self):
         """convert to json"""
         dupe = self.__dict__.copy()
+        dupe.pop('_sa_instance_state', None)
         dupe["created_at"] = str(dupe["created_at"])
         if ("updated_at" in dupe):
             dupe["updated_at"] = str(dupe["updated_at"])
