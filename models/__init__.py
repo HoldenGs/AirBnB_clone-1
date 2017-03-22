c3f5726dbc4c6fedd1a5e7db7d2a9bd4b7b2e461
Holden Grissett <holdeng96@gmail.com>
2017-03-22 03:31:14 +0000

    add storage type switching via environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -8,6 +8,8 @@ from models.state import State
 from models.review import Review
 from models.engine import file_storage
 
-
-storage = file_storage.FileStorage()
+if HBNB_TYPE_STORAGE == 'db':
+    storage = db_storage.DBStorage()
+else:
+    storage = file_storage.FileStorage()
 storage.reload()
