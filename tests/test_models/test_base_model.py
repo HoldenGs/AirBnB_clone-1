1377a8c95b0c12019241f6d33b85497dcff6677a
Holden Grissett <holdeng96@gmail.com>
2017-03-24 05:46:57 +0000

    fix db storage test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,11 @@
 import unittest
 from datetime import datetime
 from models import *
+from os import getenv
 
 
+@unittest.skipIf(getenv('HBNB_TYPE_STORAGE') == 'db',
+                 "BaseModel isn't a database table")
 class Test_BaseModel(unittest.TestCase):
     """
     Test the base model class
@@ -22,7 +25,7 @@ class Test_BaseModel(unittest.TestCase):
         self.assertIsInstance(self.model1, BaseModel)
         self.assertTrue(hasattr(self.model1, "created_at"))
         self.assertTrue(hasattr(self.model1, "id"))
-        self.assertFalse(hasattr(self.model1, "updated_at"))
+        self.assertTrue(hasattr(self.model1, "updated_at"))
 
     def test_reinstantiation(self):
         self.assertIsInstance(self.model2, BaseModel)
@@ -32,9 +35,10 @@ class Test_BaseModel(unittest.TestCase):
                          datetime(2017, 2, 10, 2, 6, 55, 258849))
 
     def test_save(self):
-        self.assertFalse(hasattr(self.model1, "updated_at"))
+        self.assertFalse(hasattr(self.model1, "name"))
+        self.model1.name = 'Ben Carson'
         self.model1.save()
-        self.assertTrue(hasattr(self.model1, "updated_at"))
+        self.assertTrue(hasattr(self.model1, "name"))
         old_time = self.model2.updated_at
         self.model2.save()
         self.assertNotEqual(old_time, self.model2.updated_at)
---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import unittest
+from os import getenv
+import os.path
+from datetime import datetime
+from models.engine.db_storage import DBStorage
+from models import *
+
+
+
+@unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db',
+                 'Storage type is not database')
+class test_DBStorage(unittest.TestCase):
+    """
+    Test database storage
+    """
+
+    @classmethod
+    def setUpClass(self):
+        storage._DBStorage__session.close()
+        self.store = DBStorage()
+        self.store.reload()
+
+    @classmethod
+    def tearDownClass(self):
+        self.store._DBStorage__session.close()
+        storage.reload()
+
+    def test_all(self):
+        self.assertEqual(len(self.store.all()), 0)
+        new_obj = State()
+        new_obj.name = 'Maine'
+        self.store.new(new_obj)
+        self.assertEqual(len(self.store.all()), 1)
+        # Make all('classname') work without console
+
+    def test_new(self):
+        new_obj = State()
+        new_obj.name = "Montana"
+        self.store.new(new_obj)
+        self.assertTrue("Montana" in self.store.all())
+        self.store.save(new_obj)
+
+    def test_save(self):
+        self.store.reload()
+        new_obj = State()
+        new_obj.name = 'Colorado'
+        self.store.new(new_obj)
+        self.store.save()
+        self.assertEqual(len(self.store.all()), 0)
+
+    def test_delete(self):
+        new_obj = State()
+        new_obj.name = "Iowa"
+        self.store.new(new_obj)
+        self.store.save()
+        self.store.delete(new_obj)
+        self.assertFalse(new_obj in self.store.all())
+
+    def test_reload(self):
+        test_len = len(self.store.all())
+        self.assertEqual(test_len, 0)
+        new_obj = City()
+        self.store.new(new_obj)
+        self.store.reload()
+        test_len = len(self.store.all())
+        self.assertEqual(test_len, 3)
