76f9861110c7f0895b4c57c3e61f08ff363aef91
Holden Grissett <holdeng96@gmail.com>
2017-03-29 02:55:45 +0000

    add file 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
