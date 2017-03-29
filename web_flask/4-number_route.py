4583091359b968654b5d9bb9a995cf43cb76c14b
Holden Grissett <holdeng96@gmail.com>
2017-03-29 03:19:00 +0000

    add file 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/number/<int:n>')
+def number_n(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/python/')  # , defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
