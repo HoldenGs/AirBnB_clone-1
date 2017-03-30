d85749ae865c2daded1740a52e35908c949e8735
Holden Grissett <holdeng96@gmail.com>
2017-03-30 04:26:17 +0000

    reduce to one method

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,26 +5,23 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states')
-def states():
+@app.route('/states', defaults={'id': None})
+@app.route('/states/<id>')
+def state_cities(id):
     states = sorted(storage.all('State').values(),
                     key=lambda state: state.name)
-    route = 'states'
-    return render_template('9-states.html',
-                           states=states, route=route)
-
-
-@app.route('/states/<string:id>')
-def state_cities(id):
-    states_list = storage.all('State').values()
-    states = None
-    for state in states_list:
-        if state.id == id:
-            states = state
-    if states:
-        route = 'state_cities'
+    if id == None:
+        route = 'states'
     else:
-        route = 'none_found'
+        states_list = states
+        states = None
+        for state in states_list:
+            if state.id == id:
+                states = state
+        if states:
+            route = 'state_cities'
+        else:
+            route = 'none_found'
     return render_template('9-states.html',
                            states=states, route=route)
 
