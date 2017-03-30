6ade30e3483ba419feebec3aa5a403963ebe3a13
Holden Grissett <holdeng96@gmail.com>
2017-03-30 03:27:30 +0000

    add file 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from os import getenv
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states')
+def states():
+    states = sorted(storage.all('State').values(),
+                    key=lambda state: state.name)
+    route = 'states'
+    return render_template('9-states.html',
+                           states=states, route=route)
+
+
+@app.route('/states/<string:id>')
+def state_cities(id):
+    states = storage.all('State').values()
+    print(states)
+    print(id)
+    states = [state for state in states if state.id == id]
+    print(states)
+    route = 'state_cities'
+    return render_template('9-states.html',
+                           states=states, route=route)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
